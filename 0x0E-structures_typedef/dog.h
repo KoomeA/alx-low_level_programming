1dee1037cf687f9d42612e21e868239cfa5364f6
KoomeA <koomeedward77@gmail.com>
2022-05-29 11:53:30 -0700

    added structure dog files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,23 @@
-#include <stdio>
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - this descripction about which dog
+ * @name: sdfasdf
+ * @age:  asdfas
+ * @owner:  asdf
+ */
+
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
-int main(void)
-{
-	struct Dog dog;
-
-	return(0);
-}
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+#endif}
